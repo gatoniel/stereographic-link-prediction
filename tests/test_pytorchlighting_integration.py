ad1af139e2e4256f2e423a1f732a8a09a48e453b
Niklas Netter <niknett@gmail.com>
2021-04-22 10:52:57 +0200

    pytorch lgihtnign test

--- tests/test_pytorchlighting_integration.py
+++ tests/test_pytorchlighting_integration.py
@@ -37,3 +37,26 @@ def test_fast_dev_run():
     trainer = pl.Trainer(fast_dev_run=True, precision=32)
     trainer.fit(module, datamodule=datamodule)
     trainer.test(module, datamodule=datamodule)
+
+
+def test_overfit():
+    hparams = SimpleNamespace(
+        encoder_layers=4,
+        encoder_hidden_dim=100,
+        decoder_layers=4,
+        decoder_hidden_dim=100,
+        discriminator_layers=1,
+        discriminator_hidden_dim=50,
+        hyperbolic_dim=2,
+        spherical_dim=2,
+        euclidean_dim=2,
+        learning_rate=1e-3,
+    )
+
+    datamodule = BacillusSubtilisSwarming("./data")
+    datamodule.prepare_data()
+    datamodule.setup("fit")
+    module = LinkPredictionModule(hparams, datamodule)
+
+    trainer = pl.Trainer(overfit_batches=10, precision=32)
+    trainer.fit(module, datamodule=datamodule)
