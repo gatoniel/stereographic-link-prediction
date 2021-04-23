a1cbdd3c7dc4eaed34c515ebe20fdb302c4d9841
Niklas Netter <niknett@gmail.com>
2021-04-23 14:33:16 +0200

    illustrate trajectories and distances

--- plot_distances.py
+++ plot_distances.py
@@ -1,4 +1,5 @@
 import torch
+from argparse import ArgumentParser
 import geoopt
 from tqdm import tqdm
 import matplotlib.pyplot as plt
@@ -11,35 +12,51 @@ from stereographic_link_prediction.Data.Distances import (
 )
 
 if __name__ == "__main__":
-    datamodule = DistancesDataModule("./data", batch_size=None)
-    datamodule.setup("fit")
-    # for i in tqdm(range(len(datamodule))):
-    #     fig, ax = plt.subplots(1, 1)
-    #     ax.scatter(datamodule.time[i], datamodule.space[i], color="red")
-    #     times = []
-    #     spaces = []
-    #     vals = []
-    #     for j in range(len(datamodule)):
-    #         if i == j:  # or j in [69]:
-    #             continue
-    #         if i < j:
-    #             vals.append(datamodule.train_dataset.distances[i, j])
-    #         else:
-    #             vals.append(datamodule.train_dataset.distances[j, i])
-    #         times.append(datamodule.time[j])
-    #         spaces.append(datamodule.space[j])
-    #     sc = ax.scatter(
-    #         times, spaces, c=vals, vmin=min(vals), vmax=max(vals), cmap="jet"
-    #     )
-    #     fig.colorbar(sc, ax=ax)
+    parser = ArgumentParser()
+    parser.add_argument("--replica", type=int, default=1)
+    args = parser.parse_args()
 
-    #     fig.savefig(f"/mnt/d/distances/{i}.png")
-    #     plt.close(fig)
+    datamodule = DistancesDataModule(
+        "./data", replica=args.replica, batch_size=None
+    )
+    datamodule.setup("fit")
+    for i in tqdm(range(len(datamodule))):
+        fig, ax = plt.subplots(1, 1)
+        ax.scatter(datamodule.time[i], datamodule.space[i], color="red")
+        times = []
+        spaces = []
+        vals = []
+        for j in range(len(datamodule)):
+            if i == j:  # or j in [69]:
+                continue
+            if i < j:
+                vals.append(datamodule.train_dataset.distances[i, j])
+            else:
+                vals.append(datamodule.train_dataset.distances[j, i])
+            times.append(datamodule.time[j])
+            spaces.append(datamodule.space[j])
+        sc = ax.scatter(
+            times, spaces, c=vals, vmin=min(vals), vmax=max(vals), cmap="jet"
+        )
+        fig.colorbar(sc, ax=ax)
+
+        fig.savefig(f"/mnt/d/distances/{i}.png")
+        plt.close(fig)
 
     fig = datamodule.plot(
         torch.tensor(datamodule.flat_mds), geoopt.PoincareBall(), True
     )
     fig.savefig(f"/mnt/d/distances/mds.png")
+    plt.close(fig)
+
+    spacetime = torch.empty((datamodule.time.shape[0], 2))
+    spacetime[:, 0] = torch.tensor(datamodule.time)
+    spacetime[:, 1] = torch.tensor(datamodule.space)
+    fig = datamodule.plot(spacetime, geoopt.PoincareBall(), True)
+    for ax in fig.axes:
+        ax.set_aspect("auto")
+    fig.savefig("/mnt/d/distances/trajectories.png")
+    plt.close(fig)
 
     indexes = torch.combinations(
         torch.arange(datamodule.flat_mds.shape[0]), r=2
